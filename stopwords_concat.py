acafbefe6ba84505169854c496ffedb3c32fdead
kim-hyeon-seop <97500492+kim-hyeon-seop@users.noreply.github.com>
2022-03-04 12:12:30 +0900

    Update stopwords_concat.py

--- stopwords_concat.py
+++ stopwords_concat.py
@@ -12,10 +12,6 @@ for data_path in data_paths:
     df_temp = pd.read_csv(data_path, index_col=0)
     df = pd.concat([df, df_temp])
 
-# 이대로 concat하면 인덱스가 없는 채로 저장된 애들 다 nan값으로 바뀌어버려.
-# 그래서 아예 따로 파일을 만들어 저장하고, df에 합쳤음.
-# :1000 한 것은 한식 데이터가 너무 많아서
-
 df.drop_duplicates(inplace=True)
 df.dropna(inplace=True)
 df.reset_index(drop=True, inplace=True)
