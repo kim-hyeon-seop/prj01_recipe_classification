ad83239e22edf7e3f4d1f100d73ac6671e44286b
kim-hyeon-seop <97500492+kim-hyeon-seop@users.noreply.github.com>
2022-03-04 12:13:12 +0900

    Update ingredient_classification_01_2_concat.py

--- ingredient_classification_01_2_concat.py
+++ ingredient_classification_01_2_concat.py
@@ -14,9 +14,6 @@ for data_path in data_paths:
     df_temp = pd.read_csv(data_path, index_col=0)
     df = pd.concat([df, df_temp[:1000]])
 
-# 이대로 concat하면 인덱스가 없는 채로 저장된 애들 다 nan값으로 바뀌어버려.
-# 그래서 아예 따로 파일을 만들어 저장하고, df에 합쳤음.
-# :1000 한 것은 한식 데이터가 너무 많아서
 
 for data_path_2 in data_paths_2:
     df_temp_2 = pd.read_csv(data_path_2)
